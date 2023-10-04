11bad79721c0af5195b6b3f122afd66572867cf8
vinay-temp <117427777+vinay-temp@users.noreply.github.com>
2023-10-05 01:16:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // (Year, Month (0-11), Day, Hour, Minute, Second)
-const startTime = new Date(2023, 9, 5, 1, 14, 45);
+const startTime = new Date(2023, 9, 5, 1, 11, 11);
 
 function updateTimer() {
   const currentTime = new Date();
